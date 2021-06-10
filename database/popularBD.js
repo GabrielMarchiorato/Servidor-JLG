e7acad56a816926ec987da9a24648760ee3d4e86
Lucasraulin <63323557+Lucasraulin@users.noreply.github.com>
2021-06-10 14:28:32 -0300

    PopularMongoDB
    Transferencia de banco de dados.

---
+++ database/popularBD.js
@@ -0,0 +1,29 @@
+require("./mongodb");
+const mongoose = require('mongoose');
+
+const accountModel = require("../models/accountModel");
+const accounts = require("./planos.json");
+
+const userModel = require("../models/userModel");
+const usuarios = require("./usuarios.json");
+
+async function carregarDados() {
+    await accountModel.deleteMany({}, async () => {
+        await accounts.forEach(async (account) => {
+            await accountModel.create(account);
+        });
+    });
+
+    await userModel.deleteMany({}, async () => {
+        await usuarios.forEach(async (usuario) => {
+            await accountModel.findOne(
+                { codigo: usuario.accountId },
+                async (err, account) => {
+                    usuario.accountId  = account.id;
+                }
+            );
+            await userModel.create(usuario);
+        });
+    });
+}
+carregarDados ();
